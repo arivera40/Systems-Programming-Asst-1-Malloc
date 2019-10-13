b3d2f38f44598828fc380007bb030d6e108e597a
arivera40 <52049534+arivera40@users.noreply.github.com>
2019-10-13 15:55:22 -0400

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -9,31 +9,56 @@ int randomize(int size){
 }
 
 int main(int argc, char *argv){
+	int workloadCount;
+	gettimeofday(&tv, NULL);
+
+	time_t workloadATime = 0;
+	time_t workloadBTime = 0;
+	time_t workloadCTime = 0;
+	time_t workloadDTime = 0;
+	time_t workloadETime = 0;
+	time_t workloadFTime = 0;
+
 	int i;
 	int j = 0;
 	int k = 0;
 	char *p;
+
+	struct timeval start, end;
+	time_t totalTime = 0;
+
 	// A: malloc() 1 byte and immediately free it - do this 150 times
-	for(i=0; i <= 150; i++){	
-		p = (char*)malloc(1);
-		free(p);
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		for(i=0; i < 150; i++){	
+			p = (char*)malloc(1);
+			free(p);
+		}
+		gettimeofday(&end, NULL);
+		workloadATime += (end - start);
 	}
+
 
 	// B: malloc() 1 byte, store the pointer in an array - do this 150 times.
  	// Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers one by one.
 	char *arr[150];
 	int count = 0;
-	for(i=0; i <= 150; i++){		// <= condition may be 1 too many
-		arr[i] = (char*)malloc(1);
-		if((i % 50) == 0){
-			for(j=j; count <= 50 ;j++){
-				free(arr[j]);
-				count++;
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		for(i=0; i < 150; i++){	
+			arr[i] = (char*)malloc(1);
+			if((i % 50) == 0){			//possible error in if and following for loop condition !!!!!
+				for(j=j; count <= 50 ;j++){
+					free(arr[j]);
+					count++;
+				}
+				count = 0;
 			}
-			count = 0;
 		}
+		gettimeofday(&end, NULL);
+		workloadBTime += (end - start);
 	}
-	
+
 	//C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
 	//	> do this until you have allocated 50 times
 	//- Keep track of each operation so that you eventually malloc() 50 bytes, in total
@@ -44,24 +69,32 @@ int main(int argc, char *argv){
 	int mallocCount = 0;
 	int freeCount = 0;
 	int random;
-	char *arr2[50];
-	while(freeCount == 50 && mallocCount == 50){
-		if(mallocCount == 50){
-			free(arr2[freeCount]);
-			freeCount++;
-		}else{
-			random = randomize(2);
-			if(random == 0){	//malloc
-				arr2[mallocCount] = (char*)malloc(1);
-				mallocCount++;
-			}else if(random == 1 && freeCount < mallocCount){	//free
+	char *arr[50];
+	
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		while(freeCount == 50 && mallocCount == 50){
+			if(mallocCount == 50){
 				free(arr2[freeCount]);
 				freeCount++;
-			}else{		//malloc, because there isn't any current pointers to free
-				arr2[mallocCount] = (char*)malloc(1);
-				mallocCount++;
+			}else{
+				random = randomize(2);
+				if(random == 0){	//malloc
+					arr2[mallocCount] = (char*)malloc(1);
+					mallocCount++;
+				}else if(random == 1 && freeCount < mallocCount){	//free
+					free(arr2[freeCount]);
+					freeCount++;
+				}else{		//malloc, because there isn't any current pointers to free
+					arr2[mallocCount] = (char*)malloc(1);
+					mallocCount++;
+				}
 			}
 		}
+		gettimeofday(&end, NULL);
+		workloadCTime += (end - start);
+		mallocCount = 0;
+		freeCount = 0;
 	}
 
 	// D: Randomly choose between a randomly-sized malloc() or free()ing a pointer  do this many
@@ -72,28 +105,47 @@ int main(int argc, char *argv){
 	// - Choose a random allocation size between 1 and 64 bytes
 	mallocCount = 0;
 	freeCount = 0;
-	int options;
-	count = 4096;
 	char *arr3[50];
-	while(freeCount == 50 && mallocCount == 50){
-		if(mallocCount == 50){
-			free(arr3[freeCount]);
-			freeCount++;
-		}else{
-			options = randomize(2);
+
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		while(freeCount == 50 && mallocCount == 50){
+			if(mallocCount == 50){
+				free(arr3[freeCount]);
+				freeCount++;
+			}else{
+				options = randomize(2);
+				if(options == 0){	//malloc
+					random = randomize(64);
+					arr3[mallocCount] = (char*)malloc(random);
+					mallocCount++;
+				}else if(options == 1 && freeCount < mallocCount){	//free
+					free(arr3[freeCount]);
+					freeCount++;	
+				}else{
+					random = randomize(64);
+					arr3[mallocCount] = (char*)malloc(random);
+					mallocCount++;
+				}
+			}
 		}
-		if(options == 0){	//malloc		
-			arr3[mallocCount] = (char*)malloc(random);
-			mallocCount++;		
-		}else if(options == 1 && freeCount < mallocCount){	//free
-			free(arr3[freeCount]);
-			freeCount++;		
-		}else{
-			arr3[mallocCount] = (char*)malloc(random);
-			mallocCount++;
-		}
-		
+		gettimeofday(&end, NULL);
+		workloadDTime += (end - start);
+		mallocCount = 0;
+		freeCount = 0;
 	}
 
-
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		//E
+		gettimeofday(&end, NULL);
+		workloadETime += (end - start);
+	}
+	
+	for(workloadCount = 0; workloadCount < 100; workloadCount++){
+		gettimeofday(&start, NULL);
+		//F
+		gettimeofday(&end, NULL);
+		workloadFTime += (end - start);
+	}
 }
