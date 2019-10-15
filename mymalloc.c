c97015420fe6e0177869d3d306c844007177fd61
arivera40 <52049534+arivera40@users.noreply.github.com>
2019-10-15 13:31:08 -0400

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stddef.h>
+#include<stdlib.h>
 #include"mymalloc.h"
 
 Metadata *head = (void*) myblock;
@@ -12,6 +13,7 @@ void initialize(){
 
 //line and function parameters will be used to throw any errors that may occur
 void *mymalloc(size_t dataSize, int line, char *file){
+	printf("dataSize = %zu & sizeof(Metadata) = %zu\n", dataSize, sizeof(Metadata));
 	if(dataSize > sizeof(myblock)){		//p = (char*)malloc(4097)
 		printf("Insufficient memory to fulfill malloc request\n filename: %s, Line: %d\n", file, line);
 		return NULL;
@@ -19,32 +21,48 @@ void *mymalloc(size_t dataSize, int line, char *file){
 	if(dataSize == 0){
 		return NULL;
 	}
-	if(head->next == NULL){
+	if(!(head->size)){
 		initialize();
+	//	printf("head->size = %zu\n", head->size);
 	}
 	
 	Metadata *ptr = head;
-	Metadata *prevPtr;
+	Metadata *prevPtr = NULL;
 	while(ptr != NULL){	//Runs loop to combine adjacent free blocks for best possible chance to allocate memory
 		if(prevPtr != NULL){
 			if(prevPtr->free == 1 && ptr->free == 1){
 				combineBlocks(prevPtr, ptr);
-				continue;
+				ptr = ptr->next;
+			}else{
+				prevPtr = ptr;
+				ptr = ptr->next;
 			}
+		}else{
 			prevPtr = ptr;
 			ptr = ptr->next;
 		}
 	}
-
+	
 	prevPtr = NULL;
 	ptr = head;
+/*	if(ptr->next == NULL){
+		printf("head->next == NULL\nhead->free == 1\n");
+	}else{
+		printf("head->next == Metadata\nhead->free == %d\n", head->free);
+	}*/
 	while(ptr != NULL){
+	//	printf("ptr->free == %d\n",head->free);
 		if(ptr->free == 1 && ptr->size > (dataSize + sizeof(Metadata))){
+	//		printf("does it enter here(1)\n");
+	//		printf("Enters insertMetadata\n");
 			insertMetadata(dataSize, ptr);
-			return (void*)((void*)ptr + sizeof(Metadata));	//website: (void*)(++ptr) in order to return correct position in front
-		}else if(ptr->free = 1 && ptr->size == dataSize){
+	//		printf("Exits insertMetadata\n");
+	//		printf("head->size = %zu (after malloc)\n", head->size);
+			return (void*)(++ptr);	//website: (void*)(++ptr) in order to return correct position in front
+		}else if(ptr->free == 1 && ptr->size == dataSize){
 			ptr->free = 0;
-			return (void*)((void*)ptr + sizeof(Metadata));
+	//		printf("does it enter here(2)\n");
+			return (void*)(++ptr);
 		}
 		prevPtr = ptr;
 		ptr = ptr->next;
@@ -59,7 +77,6 @@ void combineBlocks(Metadata *prevPtr, Metadata *ptr){
 	prevPtr->next = ptr->next;
 	prevPtr->size += ptr->size + sizeof(Metadata);
 	Metadata *tempPtr = ptr;
-	ptr = ptr->next;
 	//Need to nullify the struct tempPtr is currently pointing to
 	return;
 }
@@ -68,37 +85,31 @@ void combineBlocks(Metadata *prevPtr, Metadata *ptr){
 void insertMetadata(size_t dataSize, Metadata *prevMetadata){
 	Metadata *bookmark = (Metadata*)((void*)prevMetadata + dataSize + sizeof(Metadata));
 	bookmark->size = prevMetadata->size - dataSize - sizeof(Metadata);
+	printf("bookmark->size = %zu\n", bookmark->size);
 	bookmark->free = 1;
-	if(prevMetadata->next == NULL){
-		bookmark->next = NULL;
-	}else{
-		bookmark->next = prevMetadata->next;
-	}
+	bookmark->next = prevMetadata->next;
 	prevMetadata->next = bookmark;
+	prevMetadata->size = dataSize;
 	prevMetadata->free = 0;
 }
 
 //free will set all data back to original null value and change corresponding metadata->free = 1
 void myfree(void *ptr, int line, char *file){
 
-	Metadata *prevMetadata = (Metadata*)((char*)ptr - sizeof(Metadata));
-	printf("went in\n");
-	if(prevMetadata == NULL){
-		return;
-	}
-	printf("prevMetadata->size = %zu\n", prevMetadata->size); 
+	Metadata *prevMetadata = ptr;
+	--prevMetadata;
+
+//	printf("prevMetadata->size = %zu\n", prevMetadata->size); 
 	// Freeing address that are not pointers, (1.a)
-	//if(!(ptr exists in the region between the beginning of block array and end of block array))	(1.b)
+	//if((char*)ptr < myblock || (char*)ptr >= myblock + 4096){printf("Pointer address is out of bounds\n file: %s, line: %d", file, line}	(1.b)
 		//The ptr was never allocated, throw error, Freeing pointers that were not allocated
 	
 	//The following code will work assuming 
-/*	Metadata *prevMetadata = ptr;
-	--prevMetadata;
 	if(prevMetadata->free == 1){	//(1.c)
 		//The data ahead is already free, throw error, redundant freeing
 		printf("Redundant freeing: pointer has already been freed\nfilename: %s, line: %d\n", file, line);
 	}
-	prevMetadata->free = 1;	*/
+	prevMetadata->free = 1;	
 	
 }
 
